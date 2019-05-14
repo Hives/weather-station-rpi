f676bbade9a3058c97feedc87d316d529e0e4ace
hybridbad <hybridbad90@gmail.com>
2019-05-14 16:42:47 +0100

    Added code to read sensor

---
+++ sensor.py
@@ -0,0 +1,17 @@
+import bme280
+import smbus2
+from time import sleep
+
+port = 1
+address = 0x77
+bus = smbus2.SMBus(port)
+
+bme280.load_calibration_params(bus, address)
+
+while True:
+    sensor_data = bme280.sample(bus, address)
+    ambient_temperature = sensor_data.temperature
+    humidity = sensor_data.humidity
+    pressure = sensor_data.pressure
+    print(ambient_temperature, humidity, pressure)
+    sleep(5)
